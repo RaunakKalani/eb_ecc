c976ebce0b732e4224bb72bd8880aaec5a1f73a5
Niranjan Vaish <niranjan.vaish@in.ibm.com>
2017-05-11 00:52:00 -0500

    added header

--- Sample_Files/ecc_ff_add.v
+++ Sample_Files/ecc_ff_add.v
@@ -1,9 +1,28 @@
-module ecc_ff_add(a, b, q);
+//   (C) Copyright International Business Machines 2017
+
+// File name: ecc_ff_add.v
+//
+// Author: Niranjan Vaish (niranjan.vaish@in.ibm.com)
+//
+// Description: Sample verilog module to demonstrate 
+//              finite field addition operation.
+// 
+// Chanage log:
+// 05/10/2017 : Initial version. (Niranjan)
+//
+//
+
+
+module ecc_ff_add(clk, a, b, q);
 parameter m=163;
 
+input clk;
 input [m-1:0] a, b;
 output [m-1:0] q;
 
-assign q = a ^ b;
+reg [m-1:0] q;
+
+always @(posedge clk)
+	q = a ^ b;
 
 endmodule
--- Sample_Files/ecc_ff_add_tb.v
+++ Sample_Files/ecc_ff_add_tb.v
@@ -1,3 +1,17 @@
+//   (C) Copyright International Business Machines 2017
+
+// File name: ecc_ff_add_tb.v
+//
+// Author: Niranjan Vaish (niranjan.vaish@in.ibm.com)
+//
+// Description: Sample verilog testbench to simulate
+//              finite field addition operation.
+// 
+// Chanage log:
+// 05/10/2017 : Initial version. (Niranjan)
+//
+//
+
 module ecc_ff_add_tb;
 parameter m=163;
 parameter sample_size=56;
@@ -12,29 +26,27 @@ reg [m-1:0] q_out_array[0:sample_size-1];
 reg clk;
 integer i, j, err_cnt;
 
-ecc_ff_add #(m) inst_ecc_ff_add(a, b, q);
-
-initial
-begin
-    $readmemh("ecc_inp_a.txt", a_in_array);
-    $readmemh("ecc_inp_b.txt", b_in_array);
-    $readmemh("ecc_add.op.txt", q_out_array);
-    clk = 1'b1;
-    i = 0;
-    j = 0;
-    err_cnt = 0;
-end
+ecc_ff_add #(m) inst_ecc_ff_add(clk, a, b, q);
 
 always #10 clk = ~clk;
 
 initial 
 begin
+    $readmemh("../Testvectors/ecc_inp_a.txt", a_in_array);
+    $readmemh("../Testvectors/ecc_inp_b.txt", b_in_array);
+    $readmemh("../Testvectors/ecc_add.op.txt", q_out_array);
+    clk = 1'b1;
+    i = 0;
+    j = 0;
+    err_cnt = 0;
+
+    @(negedge clk);
+    a = a_in_array[i];
+    b = b_in_array[i];
+
     repeat(sample_size)
     begin
         @(negedge clk);
-        a = a_in_array[i];
-        b = b_in_array[i];
-        @(posedge clk);
         q_expected = q_out_array[i];
 	if (q_expected !== q)
 	begin
@@ -42,6 +54,8 @@ begin
 	    err_cnt = err_cnt + 1;
 	end
         i = i+1;
+        a = a_in_array[i];
+        b = b_in_array[i];
     end
     @(posedge clk);
     if (err_cnt == 0)
